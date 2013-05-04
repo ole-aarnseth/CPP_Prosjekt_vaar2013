df673fbacfbcea8d1f81e725ef3381474392d0dc
Vegard  Krusedokken <vegard@D531.(none)>
2013-05-04 13:56:19 +0200

    Gjort så det blir lagt til pauser på bars som ikke er helt fylt inn.

--- class_sound.cpp
+++ class_sound.cpp
@@ -19,14 +19,30 @@ void sound::playSequence(std::vector<note_t> param){
 std::vector<note_t> sound::parse(std::vector<bar> bars, int bpm){ //Gjør om til data synthen forstår. Spytter ut en vector av note structer som kan mates inn i playSequence(). 
 std::vector<note_t> output;
 
-	for(int i = 0; i < bars.size(); i++){
+	for(int i = 0; i < bars.size(); i++)
+	{
 	
-		for(int x = 0; x < bars[i].getAntT(); x++){
+		for(int x = 0; x < bars[i].getAntT(); x++)
+		{
 		note_t myNote;
 		myNote.note = f_note.calcFreq((bars[i].getNote(x).getKey()));
 		myNote.length = calculateLength(bars[i].getNote(x).getLength(), bpm);
 		output.push_back(myNote);
+
+		  
 		}
+
+		 if(bars[i].getTimeleft() < 1)
+		   {
+		     double i = bars[i].getTimeleft();
+		     double j = 1;
+		     double k = j - i;
+		     k *= 4;
+		     note_t myNote;
+		     myNote.note = f_note.calcFreq("p");
+		     myNote.length = calculateLength(k, bpm);
+		     output.push_back(myNote);
+		   }
 	}
 return output;
 }
